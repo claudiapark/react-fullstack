0beb75c41ef81753c77cbce4c41b73b900dc3238
claudiapark <claudia.park11@gmail.com>
2018-11-07 22:23:22 -0200

    Adiconar imagem de carregamento

--- Projeto/src/paginas/Home/Home.js
+++ Projeto/src/paginas/Home/Home.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import loading from './loading.svg'
 import './Home.css'
 
-function Home(props) {
-  if(!props.usuario) {
-    return <Redirect to="/login"/>
+class Home extends Component {
+  constructor (props) { 
+    super(props);
+    this.state ={ carregando: true }
+    
   }
 
-
+  
+render () {
+  if(!this.props.usuario) {
+    return <Redirect to="/login"/>
+  }
   return (
     <main className="home">
-
+    {this.state.carregando ? (
+      <img className="home__loading" src={loading} alt="Carregando" />
+    ) : (
+      <div>
+        Aqui listarei os postits amanhã
+      </div>
+    )}
     </main>
   )
+}
 }
 
 
